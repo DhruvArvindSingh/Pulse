6005a4039074dc92d83e8e3b8849338c83d5d260
DhruvArvindSingh <dhruvsinghxd@gmail.com>
2024-12-18 01:12:03 +0530

    changed databse

--- database/index.js
+++ database/index.js
@@ -4,7 +4,14 @@ dotenv.config();
 
 const { CLIENT_LINK } = process.env;
 const { Client } = pkg;
-const client = new Client(`${CLIENT_LINK}`);
+let client;
+try{
+        client = await new Client(`${CLIENT_LINK}`);
+}
+catch(e){
+        console.log("Unable to connect to database");
+        console.log(e);
+}
 await client.connect();
 await client.query("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, first_name VARCHAR(200),last_name VARCHAR(200),email VARCHAR(255) UNIQUE NOT NULL,password VARCHAR(255) NOT NULL,created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, token VARCHAR(255));");
 await client.query(`CREATE TABLE IF NOT EXISTS personal_details(
